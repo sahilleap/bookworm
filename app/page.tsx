e71c1329771f2cf748cd6801a9c1e848204c959a
sahilleap <sahil.singh@leapwallet.io>
2025-11-13 16:55:00 +0530

    Implement book search functionality with debouncing and infinite scroll in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,91 @@
-import Image from "next/image";
+"use client"
+import { useCallback, useEffect, useRef, useState } from "react";
 
 export default function Home() {
+  const [search, setSearch] = useState<string>("");
+  const [debouncedSearch, setDebouncedSearch] = useState<string>("");
+  const [books, setBooks] = useState<any[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+  const [page, setPage] = useState<number>(1);
+
+  const fetchBooks = useCallback(
+    async (_search: string, _page: number) => {
+      try {
+        const response = await fetch(`https://openlibrary.org/search.json?q=${encodeURIComponent(_search)}&page=${_page}`);
+          const data = await response.json();
+          setBooks(data.docs);
+        } catch (error) {
+          setError("Failed to fetch books. Please try again.");
+          setLoading(false);
+          setBooks([]);
+        }
+    },
+    [],
+  )
+
+  const handleSearch = async () => {
+    setLoading(true);
+    setError(null);
+    await fetchBooks(debouncedSearch, 1);
+    setLoading(false);
+  };
+
+  const onChangeText = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setSearch(e.target.value);
+    },
+    [],
+  );
+
+  useEffect(() => {
+    handleSearch()
+  }, [debouncedSearch]);
+
+  useEffect(() => {
+    const debounceTimer = setTimeout(() => {
+      setDebouncedSearch(search);
+    }, 400);
+    return () => clearTimeout(debounceTimer);
+  }, [search]);
+
+  const handleScroll = useCallback(async (e: any) => {
+    console.log("handleScroll")
+    const { scrollTop, scrollHeight, clientHeight } = e.target ;
+    if (scrollHeight - scrollTop <= clientHeight * 1.5) {
+      await fetchBooks(debouncedSearch, page + 1);
+    }
+  }, []);
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+    <div className="flex flex-col min-h-screen p-10 bg-zinc-50 font-sans dark:bg-black overflow-y-auto" onScroll={handleScroll} style={{ scrollBehavior: 'smooth' }}>
+      <div className="flex flex-row gap-2 mb-4">
+        <input 
+          type="text"
+          placeholder="Search for a book"
+          className="w-full p-2 rounded-md border border-gray-300"
+          value={search}
+          onChange={onChangeText}
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+      </div>
+      {debouncedSearch.length > 0 && (
+        <div className="flex flex-col gap-2 overflow-y-auto h-full" >
+          {loading && <div className="flex justify-center items-center h-full">Loading...</div>}
+          {error && <div className="flex justify-center items-center h-full">{error}</div>}
+          {!error && (
+            books.length === 0 ? (
+              <div className="flex justify-center items-center h-full">No books found</div>
+            ) :(
+              books.map((book: any, index: number) => 
+                <div key={index} className="flex flex-col gap-2 border-2 p-4 border-radius mb-2">
+                  <h1 className="text-xl font-bold">{book?.title}</h1>
+                  <p className="text-gray-500">{book?.author_name?.join(", ")}</p>
+                </div>
+              )
+            )
+          )}
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      )}
     </div>
   );
 }
